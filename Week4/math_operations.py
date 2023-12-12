228ef504aa8b937d1e658e6b75f8e617158eebd2
mahi7000 <mahlet7000@gmail.com>
2023-12-12 17:38:22 +0300

    maths operations

---
+++ Week4/math_operations.py
@@ -0,0 +1,34 @@
+try:
+    def basic_operations(a, b):
+        add = int(a + b)
+        subtract = int(a - b)
+        multiply = int(a * b)
+
+        try:
+            divide = int(a / b)
+        except ZeroDivisionError:
+            print("Divisor can't be Zero")
+
+
+        oper_dict = {"Addition": add, "Subtraction": subtract, "Multiplication": multiply, "Division": divide}
+
+        return (oper_dict)
+
+    def power_operation(base, exponent, **kwargs):
+        power = base ** exponent
+
+        try:
+            if "modulo" in kwargs:
+                power %= kwargs["modulo"]
+        except ZeroDivisionError:
+            print("Divisor can't be Zero")
+
+        return (power)
+
+except ValueError:
+    print("Please input only numbers")
+
+# def apply_operations(operation_list):
+#     results = list(map(lambda operation: apply_operation(*operation), operation_list))
+    
+#     return results
